63160c0aa11746da26b6a1fbaaced6448db3f5a1
Erik born <40700094+ErikBorn@users.noreply.github.com>
2023-01-30 17:25:45 -0700

    UPDATED

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -6,8 +6,6 @@ package frc.robot;
 
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
-import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.math.system.plant.DCMotor;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.drive.MecanumDrive;
@@ -20,11 +18,10 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 public class Robot extends TimedRobot {
   private static final int kFrontLeftChannel = 20;// 2;
   private static final int kRearLeftChannel = 2;// 3;
-  private static final int kFrontRightChannel = 14; //1;
+  private static final int kFrontRightChannel = 8; //1;
   private static final int kRearRightChannel = 9; // 0;
 
-  private static final int firstJoystickChannel = 0;
-  private static final int secondJoystickChannel = 0;
+  private static final int kJoystickChannel = 0;
 
   private MecanumDrive m_robotDrive;
   private Joystick m_stick;
@@ -49,11 +46,36 @@ public class Robot extends TimedRobot {
   }
 
   @Override
-  public void teleopPeriodic() 
-  {
+  public void teleopPeriodic() {
     // Use the joystick X axis for forward movement, Y axis for lateral
     // movement, and Z axis for rotation.
-    m_robotDrive.driveCartesian(-m_stick.getY(), -m_stick.getX(), -m_stick.getZ());
+    //m_robotDrive.driveCartesian(-m_stick.getY(), -m_stick.getX(), -m_stick.getZ());
+    double sin, cos, max, x, y, z;
+    x = m_stick.getX();
+    y = m_stick.getY();
+    z = m_stick.getZ();
+
+    double theta = m_stick.getDirectionRadians();
+    double power = m_stick.getMagnitude();
+
+    sin = Math.sin(theta - Math.PI/4);
+    cos = Math.cos(theta - Math.PI/4);
+
+    max = Math.max(Math.abs(sin), Math.abs(cos));
+
+    double turn = 0;
+    double leftFront2 = power * cos/max + turn;
+    double rightFront2 = power * sin/max + turn;
+    double leftRear2 = power * cos/max + turn;
+    double rightRear2 = power * sin/max + turn;
+
+    // input: theta, power, and turn
+    sin = Math.sin(theta - Math.PI/4);
+    cos = Math.cos(theta - Math.PI/4);
+    max = Math.max(Math.abs(sin),
+    Math.abs(cos));
+
+
 
     SmartDashboard.putNumber("Cartesian Y", -m_stick.getY()); 
     SmartDashboard.putNumber("Cartesian X", -m_stick.getX()); 
